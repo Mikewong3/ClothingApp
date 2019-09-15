656c666d493c3c8d6022b02f1266307083b7350d
Mike Wong <mike@Mikes-MacBook-Pro.local>
2019-09-15 00:49:22 -0400

    Added css to the html files

--- Back-End/server.js
+++ Back-End/server.js
@@ -1,74 +1,82 @@
-var express = require('express');
+var express = require("express");
 var app = express();
-app.use(express.urlencoded())
-var mysql = require('mysql');
+app.use(express.urlencoded());
+var mysql = require("mysql");
 // let connection = require("./connection");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('Front-End'))
+// app.use(express.static("Front-End"));
 let mustacheExpress = require("mustache-express");
-app.engine('html', mustacheExpress());
-app.set('view engine', 'html');
-app.set('views', __dirname + '..\\..\\Front-End');
-app.use(express.static(__dirname + "..\\..\\public"));
+app.engine("html", mustacheExpress());
+app.set("view engine", "html");
+app.set("views", "/Users/mike/documents/ClothingApp/Front-End");
+//app.set('views', __dirname + '..\\..\\Front-End');
+//app.use(express.static(__dirname + "..\\..\\public"));
+app.use(express.static("/Users/mike/documents/ClothingApp/public"));
 
 let connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'Mikewong1234',
-    database: 'firstdb'
+  host: "localhost",
+  user: "root",
+  password: "Mikewong1234",
+  database: "firstdb"
 });
 connection.connect();
 
-app.route('/')
-    .get(function (req, res) {
-        connection.query('Select * from clothing', function (err, results) {
-            if (err)
-                throw err;
-            else {
-                res.render("index", { items: results })
-            }
-        });
-    })
-app.route('/myclothing')
-    .get(function (req, res) {
-        connection.query('Select * from clothing', function (err, results) {
-            if (err)
-                throw err;
-            else {
-                res.redirect("/");
-            }
-        })
-    })
-    .post(function (req, res) {
-        console.log("CUrrently Inserting");
-        let param = [];
-        let picPath = ["C:\\Users\\jiayao.wong\\Pictures\\CLothingAppPictures\\"];
-        var output = picPath.map(function (i) { return i.replace(/\\/g, '/').replace(/^dist\//, ''); });
-        param[0] = req.body.Type;
-        param[1] = req.body.Brand;
-        param[2] = req.body.ClothingURL;
-        param[3] = output + req.body.clothingIMG
-        console.log(param);
-        connection.query('insert into clothing(type,brand,url,img) values(?,?,?,?)', param, function (err, results) {
-            if (err)
-                throw err;
-            else {
-                console.log(results);
-            }
-        })
+app.route("/").get(function(req, res) {
+  connection.query("Select * from clothing", function(err, results) {
+    if (err) throw err;
+    else {
+      res.render("index", { items: results });
+    }
+  });
+});
+app
+  .route("/myclothing")
+  .get(function(req, res) {
+    connection.query("Select * from clothing", function(err, results) {
+      if (err) throw err;
+      else {
         res.redirect("/");
+      }
     });
-app.route("/myclothing/delete/:id")
-    .get(function (req, res) {
-        console.log(req.params.id);
-        connection.query('DELETE FROM clothing where id=?', req.params.id, function (err, results) {
-            if (err)
-                throw err;
-            else {
-                console.log(results);
-            }
-        })
-        res.redirect("/");
+  })
+  .post(function(req, res) {
+    console.log("CUrrently Inserting");
+    let param = [];
+    let picPath = ["C:\\Users\\jiayao.wong\\Pictures\\CLothingAppPictures\\"];
+    var output = picPath.map(function(i) {
+      return i.replace(/\\/g, "/").replace(/^dist\//, "");
     });
+    param[0] = req.body.Type;
+    param[1] = req.body.Brand;
+    param[2] = req.body.ClothingURL;
+    param[3] = output + req.body.clothingIMG;
+    console.log(param);
+    connection.query(
+      "insert into clothing(type,brand,url,img) values(?,?,?,?)",
+      param,
+      function(err, results) {
+        if (err) throw err;
+        else {
+          console.log(results);
+        }
+      }
+    );
+    res.redirect("/");
+  });
+app.route("/myclothing/delete/:id").get(function(req, res) {
+  console.log(req.params.id);
+  console.log("--Deleted");
+
+  connection.query("DELETE FROM clothing where id=?", req.params.id, function(
+    err,
+    results
+  ) {
+    if (err) throw err;
+    else {
+      console.log(results);
+    }
+  });
+  res.redirect("/");
+});
 app.listen(3000);
--- public/index.js
+++ public/index.js
@@ -1,19 +1,23 @@
 function getPictureDetails() {
-    //alert("The form was submitted");
-    //console.log(document.getElementById('Type').value);
-    var file = document.getElementById('clothingIMG').files[0];
-    console.log(file);
+  //alert("The form was submitted");
+  //console.log(document.getElementById('Type').value);
+  var file = document.getElementById("clothingIMG").files[0];
+  console.log(file);
 }
 function loadFile(event) {
-    var output = document.getElementById('output');
-    output.src = URL.createObjectURL(event.target.files[0]);
-}
-function deleteItem() {
-    alert("It worke");
-    let id = $('#id');
-    console.log(id);
+  var output = document.getElementById("output");
+  output.src = URL.createObjectURL(event.target.files[0]);
 }
 function showData() {
-    alert("It works");
-    console.log("WOrks?");
+  alert("It works");
+  console.log("WOrks?");
+}
+
+function deleteItem(id) {
+  console.log(id);
+  const HTTP = new XMLHttpRequest();
+  const url = "./myclothing/delete/" + id;
+  HTTP.open("GET", url);
+  HTTP.send();
+  HTTP.onreadystatechange = function(res) {};
 }
